5859a28f1ee334d1040425d66a7b7b6186808ff3
Dieter Peirs <github@dieterpeirs.com>
2023-10-24 13:25:57 +0200

    update sass processing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,18 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    postcssOptions: {
-      compile: {
-        enabled: true,
-        extension: 'scss',
-        parser: require('postcss-scss'),
-        plugins: [
-          {
-            module: require('@csstools/postcss-sass'),
-          }
-        ],
-      },
-    },
     '@appuniversum/ember-appuniversum': {
       disableWormholeElement: true,
     },
