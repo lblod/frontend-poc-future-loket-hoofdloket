dc4b6455c1e370e375a81210313a25757dea9965
Erika Pauwels <erika.pauwels@gmail.com>
2024-05-09 12:22:20 +0200

    Add fixed filters on executing authority level and target audience on dashboard

---
+++ app/config/constants.js
@@ -0,0 +1,10 @@
+export default {
+  TARGET_AUDIENCES: {
+    LOCAL_GOVERNMENT: 'https://productencatalogus.data.vlaanderen.be/id/concept/Doelgroep/LokaalBestuur',
+    ORGANIZATION: 'https://productencatalogus.data.vlaanderen.be/id/concept/Doelgroep/Organisatie'
+  },
+  EXECUTING_AUTHORITY_LEVELS: {
+    FLEMISH: 'https://productencatalogus.data.vlaanderen.be/id/concept/UitvoerendBestuursniveau/Vlaams',
+    PROVINCE: 'https://productencatalogus.data.vlaanderen.be/id/concept/UitvoerendBestuursniveau/Provinciaal'
+  }
+}
--- app/routes/dashboard/index.js
+++ app/routes/dashboard/index.js
@@ -1,5 +1,8 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import constants from '../../config/constants';
+
+const { TARGET_AUDIENCES, EXECUTING_AUTHORITY_LEVELS } = constants;
 
 export default class DashboardIndexRoute extends Route {
   @service store;
@@ -11,15 +14,27 @@ export default class DashboardIndexRoute extends Route {
     size: {
       refreshModel: true
     }
-   };
+  };
 
-  model(params) {
+  async model(params) {
+    const targetAudiences = await Promise.all([
+      this.store.findRecordByUri('concept', TARGET_AUDIENCES.LOCAL_GOVERNMENT),
+      this.store.findRecordByUri('concept', TARGET_AUDIENCES.ORGANIZATION)
+    ]);
+
+    const executingAuthorityLevels = await Promise.all([
+      this.store.findRecordByUri('concept', EXECUTING_AUTHORITY_LEVELS.FLEMISH),
+      this.store.findRecordByUri('concept', EXECUTING_AUTHORITY_LEVELS.PROVINCE)
+    ]);
+
     return this.store.query('public-service', {
       include: 'type,thematic-areas',
       page: {
         number: params.page,
         size: params.size
-      }
+      },
+      'filter[target-audiences][:id:]': targetAudiences.map((c) => c.id).join(','),
+      'filter[executing-authority-levels][:id:]': executingAuthorityLevels.map((c) => c.id).join(',')
     });
   }
 }
