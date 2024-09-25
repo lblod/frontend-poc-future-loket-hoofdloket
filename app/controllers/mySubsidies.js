0fb93c6184312839f953a1c2cda15b6cc84f6848
Benjamin Palmero <palmerobenjamin@gmail.com>
2024-09-25 11:15:58 +0200

    update content + create dossier-missing-info

--- app/controllers/mySubsidies.js
+++ app/controllers/mySubsidies.js
@@ -1,13 +1,14 @@
 export const mySubsidiesData = [
   {
-    name: 'Fietsinfrastructuur',
+    name: 'Bossubsidie Molenstraat',
     description:
       'Voor investeringen op of aan gemeentelijke wegen ontvangen de lokale besturen een subsidie die één derde tot maximaal de helft bedraagt van de kostprijs van die investeringsprojecten. De Vlaamse Regering legde op 16 december 2022 de maximale subsidie vast voor het Kopenhagenplan. De 218 lokale besturen die projecten aanmeldden, krijgen samen 112,5 miljoen euro toegewezen.',
     status: 'Actief - Aanvraag uitbetaling',
-    deadline: '15/09/25',
-    lastUpdate: '12/08/24',
+    deadline: '15 september 2025',
+    lastUpdate: '12 augustus 24',
     link: 'https://dev.loket.lblod.info/subsidy/applications/6576E8B1D9E01244D47144E9/steps/5f6193e5-7427-4ec8-90b1-636ed5181c19/forms/6576E8D1D9E01244D47144EB',
-    theme: 'Financieel voordeel',
+    type: 'Financieel voordeel',
+    service: "Subsidie voor investeringen in fietssnelwegen",
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
@@ -19,10 +20,11 @@ export const mySubsidiesData = [
     description:
       'Subsidies aan de Vlaamse gemeenten voor de begeleiding van en het creëren van bijkomende opvangplaatsen voor tijdelijk ontheemden uit Oekraïne.',
     status: 'Actief',
-    deadline: '31/05/25',
-    lastUpdate: '02/06/24',
+    deadline: '31 mei 2025',
+    lastUpdate: '02 juni 2024',
     link: 'https://dev.loket.lblod.info/subsidy/applications/6595BBFDD9E01244D4714541/steps/c3b91825-1d48-4134-a5ff-7ace88dff122/forms/6595BC04D9E01244D4714542',
-    theme: 'Financieel voordeel',
+    type: 'Financieel voordeel',
+    service: "Subsidie voor investeringen in fietssnelwegen",
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
@@ -34,10 +36,11 @@ export const mySubsidiesData = [
     description:
       'De lokale besturen zijn de grote actor in de creatie van opvangplaatsen voor mensen op de vlucht uit Oekraïne. De Vlaamse overheid biedt financiële ondersteuning aan de lokale besturen voor de door hen gemaakte kosten voor de opvang van deze tijdelijk ontheemden.',
     status: 'Concept',
-    deadline: '31/05/25',
-    lastUpdate: '26/08/24',
+    deadline: '31 mei 2025',
+    lastUpdate: '26 augustus 2024',
     link: 'https://dev.loket.lblod.info/subsidy/applications/6578620ED9E01244D471451C/steps/74d52acc-06a8-44ef-8cc7-a069c0ea0413/forms/65786213D9E01244D471451D',
-    theme: 'Financieel voordeel',
+    type: 'Financieel voordeel',
+    service: "Subsidie voor investeringen in fietssnelwegen",
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
@@ -49,10 +52,11 @@ export const mySubsidiesData = [
     description:
       'Op 15 juli 2022 besliste de Vlaamse Regering om de Vlaamse gemeenten en steden en de Vlaamse Gemeenschapscommissie (VGC) te ondersteunen bij de uitrol van een lokaal e-inclusiebeleid. Deze subsidie kadert binnen het actieplan Iedereen Digitaal, dat onderdeel is van het relanceplan Vlaamse Veerkracht. Het doel van Iedereen Digitaal is dat ieder lokaal bestuur tegen 2024 een eigen lokale digitale inclusiewerking heeft.',
     status: 'Concept',
-    deadline: '30/06/24',
-    lastUpdate: '12/08/24',
+    deadline: '30 juni 2024',
+    lastUpdate: '12 augustus 2024',
     link: 'https://dev.loket.lblod.info/subsidy/applications/63315E1CAFED6840FABA4EF8/steps/c38d4ab1-17b0-4281-81d7-7bd0001f6053/forms/63315E1EAFED6840FABA4EF9',
-    theme: 'Financieel voordeel',
+    type: 'Financieel voordeel',
+    service: "Subsidie voor investeringen in fietssnelwegen",
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
