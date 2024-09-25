cce4e5ef260185bebc0ec8f615ece6cdff9cba93
Benjamin Palmero <palmerobenjamin@gmail.com>
2024-09-25 11:40:34 +0200

    update with more coherent data

--- app/controllers/mySubsidies.js
+++ app/controllers/mySubsidies.js
@@ -2,13 +2,13 @@ export const mySubsidiesData = [
   {
     name: 'Bossubsidie Molenstraat',
     description:
-      'Voor investeringen op of aan gemeentelijke wegen ontvangen de lokale besturen een subsidie die één derde tot maximaal de helft bedraagt van de kostprijs van die investeringsprojecten. De Vlaamse Regering legde op 16 december 2022 de maximale subsidie vast voor het Kopenhagenplan. De 218 lokale besturen die projecten aanmeldden, krijgen samen 112,5 miljoen euro toegewezen.',
+      'Voor de aanleg van een bos met inheemse soorten, eventueel gecombineerd met populier, kunt u een subsidie krijgen. De subsidie geldt zowel voor de aanplant, als voor natuurlijke verjonging. De bebossing mag gedurende een periode van 25 jaar na de uitbetaling van de subsidie niet worden ontbost.',
     status: 'Actief - Aanvraag uitbetaling',
     deadline: '15 september 2025',
     lastUpdate: '12 augustus 24',
     link: 'https://dev.loket.lblod.info/subsidy/applications/6576E8B1D9E01244D47144E9/steps/5f6193e5-7427-4ec8-90b1-636ed5181c19/forms/6576E8D1D9E01244D47144EB',
     type: 'Financieel voordeel',
-    service: "Subsidie voor investeringen in fietssnelwegen",
+    service: 'Subsidie voor bebossing',
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
@@ -24,7 +24,7 @@ export const mySubsidiesData = [
     lastUpdate: '02 juni 2024',
     link: 'https://dev.loket.lblod.info/subsidy/applications/6595BBFDD9E01244D4714541/steps/c3b91825-1d48-4134-a5ff-7ace88dff122/forms/6595BC04D9E01244D4714542',
     type: 'Financieel voordeel',
-    service: "Subsidie voor investeringen in fietssnelwegen",
+    service: 'Subsidies voor de opbouw van opvangcapaciteit naar aanleiding van de Oekraïnecrisis',
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
@@ -40,7 +40,7 @@ export const mySubsidiesData = [
     lastUpdate: '26 augustus 2024',
     link: 'https://dev.loket.lblod.info/subsidy/applications/6578620ED9E01244D471451C/steps/74d52acc-06a8-44ef-8cc7-a069c0ea0413/forms/65786213D9E01244D471451D',
     type: 'Financieel voordeel',
-    service: "Subsidie voor investeringen in fietssnelwegen",
+    service: 'Subsidie voor investeringen in fietssnelwegen',
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
@@ -56,7 +56,7 @@ export const mySubsidiesData = [
     lastUpdate: '12 augustus 2024',
     link: 'https://dev.loket.lblod.info/subsidy/applications/63315E1CAFED6840FABA4EF8/steps/c38d4ab1-17b0-4281-81d7-7bd0001f6053/forms/63315E1EAFED6840FABA4EF9',
     type: 'Financieel voordeel',
-    service: "Subsidie voor investeringen in fietssnelwegen",
+    service: 'Subsidie voor investeringen in fietssnelwegen',
     id: '6276 - 47040242545',
     provider: 'Agentschap Binnenlands Bestuur',
     details:
