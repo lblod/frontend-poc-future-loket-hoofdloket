bdbd97f0936d37c7614adabcb5b82ae28fd7bf4a
Benjamin Palmero <palmerobenjamin@gmail.com>
2025-01-27 14:58:45 +0100

    Remove option to sort Z-A

--- app/controllers/dashboard/index.js
+++ app/controllers/dashboard/index.js
@@ -18,7 +18,7 @@ export default class DashboardIndexController extends Controller {
   sortingOptions = [
     // { label: 'Deadline', value: 'end-date' },
     { label: 'A-Z (oplopend)', value: ':no-case:name' },
-    { label: 'Z-A (aflopend)', value: '-:no-case:name' },
+    // { label: 'Z-A (aflopend)', value: '-:no-case:name' },
     { label: 'Nieuwste', value: 'date-created' },
   ];
 
