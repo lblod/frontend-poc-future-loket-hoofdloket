1411ad40b86c4993f0dc4c33226dee2af2273d2f
Erika Pauwels <erika.pauwels@gmail.com>
2025-02-22 13:26:19 +0100

    Fetch services to show on landing page from store

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,19 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
+
+  model() {
+    // TODO add filter on is-favorite
+    return this.store.query('public-service', {
+      page: {
+        size: 10,
+        number: 0
+      }
+    });
+  }
+
   setupController(controller) {
     super.setupController(...arguments);
     controller.searchTerm = null;
