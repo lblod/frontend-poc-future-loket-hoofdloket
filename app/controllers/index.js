42c51e266516e90262941e5af63e125de1db61e5
Erika Pauwels <erika.pauwels@gmail.com>
2025-02-22 13:16:37 +0100

    Implement search on landing page

---
+++ app/controllers/index.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class IndexController extends Controller {
+  @service router;
+
+  @tracked searchTerm;
+  suggestions = ["Suggestion", "Suggestion", "Suggestion", "Suggestion"];
+
+  @action
+  search(e) {
+    e.preventDefault();
+    this.router.transitionTo('products', {
+      queryParams: {
+        page: 0,
+        searchTerm: this.searchTerm
+      }
+    });
+  }
+
+  @action
+  updateSearchTerm(e) {
+    this.searchTerm = e.target.value;
+  }
+
+  @action
+  resetSearch() {
+    this.searchTerm = null;
+  }
+}
