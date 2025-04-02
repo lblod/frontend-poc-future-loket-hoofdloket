a3bc5be15b1a4b4fbef1dcf125dc1565d0a02741
Erika Pauwels <erika.pauwels@gmail.com>
2025-04-02 16:06:44 +0200

    Move hardcoded favorites on landing page to model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,13 +5,111 @@ export default class IndexRoute extends Route {
   @service store;
 
   model() {
+    return [
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/berichtencentrum",
+        },
+        name: { default: "Berichtencentrum" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/beleids-en-beheerscyclus-digitale-rapportering-bbc-dr",
+        },
+        name: { default: "BBC-DR" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/mandatenbeheer-lokale-besturen",
+        },
+        name: { default: "Mandatenbeheer" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/subsidiebeheer",
+        },
+        name: { default: "SubsidiePunt" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/data-en-tools/databank-erediensten",
+        },
+        name: { default: "Databank Erediensten" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/organisatie-en-werking/loket-voor-lokale-besturen",
+        },
+        name: { default: "Verenigingen" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/berichtencentrum",
+        },
+        name: { default: "Berichtencentrum" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/beleids-en-beheerscyclus-digitale-rapportering-bbc-dr",
+        },
+        name: { default: "BBC-DR" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/mandatenbeheer-lokale-besturen",
+        },
+        name: { default: "Mandatenbeheer" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/loket-voor-lokale-besturen/subsidiebeheer",
+        },
+        name: { default: "SubsidiePunt" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/data-en-tools/databank-erediensten",
+        },
+        name: { default: "Databank Erediensten" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/organisatie-en-werking/loket-voor-lokale-besturen",
+        },
+        name: { default: "Verenigingen" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/data-en-tools/databank-erediensten",
+        },
+        name: { default: "Databank Erediensten" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/organisatie-en-werking/loket-voor-lokale-besturen",
+        },
+        name: { default: "Verenigingen" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/data-en-tools/databank-erediensten",
+        },
+        name: { default: "Databank Erediensten" }
+      },
+      {
+        website: {
+          url: "https://www.vlaanderen.be/lokaal-bestuur/organisatie-en-werking/loket-voor-lokale-besturen",
+        },
+        name: { default: "Verenigingen" }
+      },
+    ];
     // TODO add filter on is-favorite
-    return this.store.query('public-service', {
-      page: {
-        size: 10,
-        number: 0
-      }
-    });
+    // return this.store.query('public-service', {
+    //   page: {
+    //     size: 10,
+    //     number: 0
+    //   }
+    // });
   }
 
   setupController(controller) {
